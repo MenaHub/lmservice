ae6fbd6adf609c34f9d4112e269d4bfe95776061
n.menarbin <n.menarbin@reply.it>
2024-05-16 23:19:35 +0200

    refactory userEnquiryRepository

--- l7e-backend/src/main/java/it/lmservice/ecommerce/repository/UserEnquiryRepository.java
+++ l7e-backend/src/main/java/it/lmservice/ecommerce/repository/UserEnquiryRepository.java
@@ -3,5 +3,5 @@ package it.lmservice.ecommerce.repository;
 import it.lmservice.ecommerce.entity.UserEnquiryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserEnquiryEntityRepository extends JpaRepository<UserEnquiryEntity, Long> {
+public interface UserEnquiryRepository extends JpaRepository<UserEnquiryEntity, Long> {
 }
--- l7e-backend/src/main/java/it/lmservice/ecommerce/service/UserService.java
+++ l7e-backend/src/main/java/it/lmservice/ecommerce/service/UserService.java
@@ -2,7 +2,7 @@ package it.lmservice.ecommerce.service;
 
 import it.lmservice.ecommerce.controller.dto.UserEnquiryBean;
 import it.lmservice.ecommerce.entity.UserEnquiryEntity;
-import it.lmservice.ecommerce.repository.UserEnquiryEntityRepository;
+import it.lmservice.ecommerce.repository.UserEnquiryRepository;
 import it.lmservice.ecommerce.mapper.UserEnquiryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,13 +14,13 @@ public class UserService {
     @Autowired
     private UserEnquiryMapper userEnquiryMapper;
     @Autowired
-    private UserEnquiryEntityRepository userEnquiryEntityRepository;
+    private UserEnquiryRepository userEnquiryRepository;
 
     @Transactional
     public void createUserEnquiry(UserEnquiryBean userEnquiryBean) {
         //TODO: save user enquiry to database
         UserEnquiryEntity userEnquiryEntity = userEnquiryMapper.toClientEnquiryEntity(userEnquiryBean);
-        userEnquiryEntityRepository.save(userEnquiryEntity);
+        userEnquiryRepository.save(userEnquiryEntity);
         //TODO: send email to admin
     }
 }
