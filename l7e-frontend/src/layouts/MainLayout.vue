a9099fcc57f580e8285879a7acc94256aa23d9ac
n.menarbin <n.menarbin@reply.it>
2024-06-09 19:00:01 +0200

    main layout fix

--- l7e-frontend/src/layouts/MainLayout.vue
+++ l7e-frontend/src/layouts/MainLayout.vue
@@ -1,56 +1,28 @@
 <template>
-  <q-layout view="hHh LpR fff" style="min-height: 100vh">
-    <q-header elevated>
-      <q-toolbar>
+  <q-layout class="gradient-background" view="hHh LpR fff" style="min-height: 100vh">
+    <q-header class="gradient-background" style="min-height:fit-content">
+      <q-toolbar class="flex-center q-pa-md" style="flex-wrap:wrap; gap:10px">
         <q-btn
+          class="text-black"
+          style="font-size: medium; font-weight: 300; min-width: fit-content;"
+          v-for="route in internalRoutes"
           flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleMenuDrawer"
+          unelevated
+          rounded
+          no-caps
+          :ripple="false"
+          :label="route.title"
+          :key="route.title"
+          :to="`/${route.link}`"
         />
-
-        <q-toolbar-title class="col">
-          <q-breadcrumbs>
-            <template v-slot:separator>
-              <q-icon size="1.2em" name="chevron_right" />
-            </template>
-            <q-breadcrumbs-el class="text-white" label="L.M. Service" to="/" />
-            <q-breadcrumbs-el
-              v-if="getCurrentRoute()"
-              :label="getCurrentRoute()?.title"
-              :to="
-                getCurrentRoute()?.link == '/'
-                  ? '/'
-                  : `/${getCurrentRoute()?.link}`
-              "
-            />
-            />
-          </q-breadcrumbs>
-        </q-toolbar-title>
-        <!-- <q-input
-          class="col q-mx-sm"
-          dense
-          clearable
-          borderless
-          placeholder="Search"
-          type="search"
-          clear-icon="close"
-          :input-style="{ color: 'white' }"
-          v-model="search"
-        >
-          <template v-slot:prepend>
-            <q-icon name="search" color="white" />
-          </template>
-        </q-input> -->
         <q-btn
+          color="black"
           flat
-          dense
           round
+          size="md"
           icon="shopping_cart"
           aria-label="Shopping Cart"
-          @click="toogleCartDrawer"
+          :to="'/shopping-cart'"
         >
           <q-badge
             v-if="shoppingCart.length > 0"
@@ -60,7 +32,7 @@
             :label="shoppingCart.length"
           />
         </q-btn>
-        <q-btn
+        <!-- <q-btn
           class="q-ml-sm"
           flat
           dense
@@ -68,113 +40,12 @@
           icon="account_circle"
           aria-label="Account"
           @click="toogleAccountDrawer"
-        />
+        /> -->
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      class="q-pt-sm"
-      overlay
-      v-model="menuDrawerOpen"
-      elevated
-      ref="menuDrawer"
-    >
-      <q-list>
-        <q-item-label header> Menu </q-item-label>
-        <q-item
-          v-for="route in internalRoutes"
-          :key="route.title"
-          clickable
-          :active="`/${route.link}` === $route.path"
-          :to="`/${route.link}`"
-          active-class="bg-primary text-white"
-        >
-          <q-item-section v-if="route.icon" avatar>
-            <q-icon :name="route.icon" />
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>{{ route.title }}</q-item-label>
-            <!-- <q-item-label caption>{{ route.caption }}</q-item-label> -->
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
-
-    <q-drawer
-      class="q-pt-sm"
-      side="right"
-      overlay
-      v-model="cartDrawerOpen"
-      elevated
-      ref="cartDrawer"
-    >
-      <div v-if="shoppingCart.length > 0">
-        <q-list>
-          <q-item>
-            <q-item-section>
-              <div class="text-h6 text-center">Shopping cart</div>
-            </q-item-section>
-          </q-item>
-          <q-item v-for="item in shoppingCart" :key="item.id">
-            <q-item-section class="col-6 q-ml-sm">
-              <q-item-label class="ellipsis">{{ item.name }}</q-item-label>
-              <q-item-label> {{ '€ ' + item.price }}</q-item-label>
-            </q-item-section>
-            <q-item-section class="flex flex-center">
-              <q-icon
-                v-if="item.quantity > 1"
-                class="cursor-pointer"
-                name="remove"
-                @click="item.quantity--"
-              />
-            </q-item-section>
-            <q-item-section class="flex flex-center">
-              {{ item.quantity }}
-            </q-item-section>
-            <q-item-section class="flex flex-center">
-              <q-icon
-                class="cursor-pointer"
-                name="add"
-                @click="item.quantity++"
-              />
-            </q-item-section>
-            <q-item-section class="flex flex-center">
-              <q-btn
-                flat
-                dense
-                round
-                color="red-12"
-                icon="delete"
-                aria-label="Delete"
-                @click="removeItemFromCart(item)"
-              />
-            </q-item-section>
-          </q-item>
-        </q-list>
-        <q-separator size="2px" />
-        <q-item>
-          <q-item-section class="col-6 q-ml-sm">
-            <q-item-label class="text-h6">Total</q-item-label>
-          </q-item-section>
-          <q-item-section class="flex flex-center">
-            <q-item-label class="text-h6">
-              {{ '€ ' + getCartTotal }}
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-      </div>
-      <div v-else class="column full-height flex-center">
-        <div class="row text-center q-pa-sm">
-          <q-icon name="remove_shopping_cart" size="8rem" color="grey-5" />
-        </div>
-        <div class="row text-center">
-          <p class="text-h6 text-bold">No item in the cart</p>
-        </div>
-      </div>
-    </q-drawer>
-
-    <q-drawer
+    <!-- TODO: refactor into a drop down dialog -->
+    <!-- <q-drawer
       class="q-pt-sm"
       side="right"
       overlay
@@ -258,13 +129,28 @@
           </q-item-section>
         </q-item>
       </q-list>
-    </q-drawer>
+    </q-drawer> -->
 
-    <q-page-container class="flex justify-center">
-      <router-view class="q-pa-xl" style="max-width: 1280px; width: 90%" />
+    <q-page-container class="flex justify-center relative">
+      <router-view class="q-pa-xl" style="max-width: 1280px; width: 90%"/>
+        <q-btn
+          class="favorite-btn"
+          v-if="getFavoritesNumber() > 0"
+          round
+          push
+          icon="favorite"
+          :to="'/favorites'"
+        >
+          <q-badge
+            color="red"
+            rounded
+            floating
+            :label="getFavoritesNumber()"
+          />
+        </q-btn>
     </q-page-container>
 
-    <q-footer elevated>
+    <q-footer class="bg-transparent">
       <q-toolbar>
         <q-toolbar-title class="row flex-center">
           L.M. Service - Copyright {{ new Date().getFullYear() }}
@@ -276,114 +162,106 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { CartItem } from 'src/components/models';
+import { useFavoritesStore } from 'src/stores/favorites-store';
 import { useCartStore } from 'src/stores/cart-store';
-import { GoogleLogin, googleLogout } from 'vue3-google-login';
 const cartStore = useCartStore();
+//import { GoogleLogin, googleLogout } from 'vue3-google-login';
+
+const favoritesStore = useFavoritesStore();
+
 
 export default defineComponent({
   name: 'MainLayout',
-  components: { GoogleLogin },
+  //components: { GoogleLogin },
   data() {
     return {
       customLogin: process.env.CUSTOM_LOGIN_ENABLED === 'true',
       userLoggedIn: false,
       showPassword: false,
-      menuDrawerOpen: false,
       cartDrawerOpen: false,
       accountDrawerOpen: false,
+      shoppingCart: cartStore.cartItems,
       internalRoutes: [
         {
-          title: 'About',
-          icon: 'home',
+          title: 'Home',
           link: '',
         },
         {
           title: 'Gallery',
-          icon: 'image',
           link: 'gallery',
         },
         {
           title: 'Services',
-          icon: 'handyman',
           link: 'services',
         },
         {
           title: 'Shop',
-          icon: 'store',
           link: 'shop',
         },
         {
-          title: 'Favorites',
-          icon: 'favorite',
-          link: 'favorites',
-        },
-        {
           title: 'Contacts',
-          icon: 'contacts',
           link: 'contacts',
         },
-        {
-          title: 'Book a consultation',
-          icon: 'event',
-          link: 'book-a-consultation',
-        },
+        // {
+        //   title: 'Book a consultation',
+        //   link: 'book-a-consultation',
+        // },
       ],
-      shoppingCart: cartStore.cartItems,
       username: '',
       password: '',
     };
   },
   methods: {
-    toggleMenuDrawer() {
-      this.menuDrawerOpen = !this.menuDrawerOpen;
+    getFavoritesNumber() {
+      return favoritesStore.favorites.length;
     },
     toogleCartDrawer() {
       this.accountDrawerOpen = false;
       this.cartDrawerOpen = !this.cartDrawerOpen;
     },
-    toogleAccountDrawer() {
-      this.cartDrawerOpen = false;
-      this.accountDrawerOpen = !this.accountDrawerOpen;
-    },
-    removeItemFromCart(item: CartItem) {
-      cartStore.removeItemFromCart(item);
-    },
+    // toogleAccountDrawer() {
+    //   this.cartDrawerOpen = false;
+    //   this.accountDrawerOpen = !this.accountDrawerOpen;
+    // },
     getCurrentRoute() {
       let route = this.internalRoutes
         .slice(1)
         .find((route) => this.$route.path.includes(`/${route.link}`));
       return route ? route : null;
     },
-    callback(response: any) {
-      //console.log("Response", response)
-      console.log("Access token", response.access_token)
-      //validateToken({accessToken: response.access_token})
-    },
-    userLogout(){
-      this.userLoggedIn = false;
-      googleLogout();
-    },
-    classicLogin() {
-      // TODO: add standard login logic here
-      console.log('Logging in with username:', this.username, 'and password:', this.password);
-      // Example: You may use an authentication service to authenticate the user
-    },
-    loginWithGoogle() {
-      // TODO: add Google login logic here
-      console.log('Logging in with Google');
-      // TODO: use OAuth2 authentication with Google APIs
-
-
-    },
-  },
-  computed: {
-    getCartTotal(): number {
-      return this.shoppingCart.reduce(
-        (acc, item) => acc + item.price * item.quantity,
-        0
-      );
-    },
+    // callback(response: any) {
+    //   //console.log("Response", response)
+    //   console.log("Access token", response.access_token)
+    //   //validateToken({accessToken: response.access_token})
+    // },
+    // userLogout(){
+    //   this.userLoggedIn = false;
+    //   googleLogout();
+    // },
+    // classicLogin() {
+    //   // TODO: add standard login logic here
+    //   console.log('Logging in with username:', this.username, 'and password:', this.password);
+    //   // Example: You may use an authentication service to authenticate the user
+    // },
+    // loginWithGoogle() {
+    //   // TODO: add Google login logic here
+    //   console.log('Logging in with Google');
+    //   // TODO: use OAuth2 authentication with Google APIs
+    // },
   },
 });
 </script>
+<style lang="scss">
+.gradient-background {
+  background: linear-gradient(to right, $primary, white);
+}
+
+.favorite-btn {
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  z-index: 1000;
+  max-width: 20px;
+  max-height: 20px;
+}
+</style>
